ab5773860bdff04864893d32162d5b50579afb82
Robert Seligmann <robert.seligmann@epassi.com>
2021-02-21 16:06:18 +0200

    switched tailwind for styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,17 @@
 import React from "react";
-import Head from "next/head";
+
 import { ThemeProvider } from "next-themes";
 import { MDXProvider } from "@mdx-js/react";
 import MDXComponents from "@/components/MDXComponents";
-import "@/styles/main.css";
-
+import "tailwindcss/tailwind.css";
+import { GlobalStyle } from '@/components/Global'
 require("prismjs/themes/prism-tomorrow.css");
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
-      <div className="layout">
-        <Head>
-          <title>My page title</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
+      <GlobalStyle/>
+      <div>
         <MDXProvider components={MDXComponents}>
           <Component {...pageProps} />
         </MDXProvider>
